e197a9497a8c91ca6b7d99624c36214196289682
ryancolclough <ryancolclough2@gmail.com>
2026-05-06 19:07:04 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+Upload these files directly to the site root:
+index.html
+style.css
+script.js
+assets/
+
+Do not place them inside another folder.
---
+++ script.js
@@ -0,0 +1,1 @@
+const b=document.getElementById('menuBtn');const n=document.getElementById('mainNav');if(b&&n){b.addEventListener('click',()=>n.classList.toggle('open'));}
