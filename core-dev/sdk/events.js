48adc74bc6dcd96f8798667963f3a6c2abad484b
ryancolclough <ryancolclough2@gmail.com>
2026-07-12 01:16:30 -0400

    Delete core-dev/sdk/events.js

--- core-dev/sdk/events.js
+++
@@ -1,9 +0,0 @@
-export class EventBus {
-  constructor(){ this.events = new Map(); }
-  on(name, handler){
-    if(!this.events.has(name)) this.events.set(name, new Set());
-    this.events.get(name).add(handler);
-    return () => this.events.get(name)?.delete(handler);
-  }
-  emit(name, detail){ this.events.get(name)?.forEach(fn => fn(detail)); }
-}
